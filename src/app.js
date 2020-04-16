c045155721f3e3bbca1eac77dfc447a89d2bd100
sanjhipandey <41279537+sanjhipandey@users.noreply.github.com>
2020-04-16 10:30:05 +0530

    Setup app fpr heroku

--- src/app.js
+++ src/app.js
@@ -14,6 +14,7 @@ const forecast = require("./utils/forecast")
 
 
 const app = express()
+const port = process.env.PORT || 3000 //env is object that access environmet variables
 
 // Define path for Express config
 const publicDirectoryPath = path.join(__dirname, '../public')
@@ -127,6 +128,6 @@ app.get('*', (req, res)=>{
 
 })
 
-app.listen(3000, () => {
+app.listen(port, () => {
     console.log('server is up on port 3000.')
 })
